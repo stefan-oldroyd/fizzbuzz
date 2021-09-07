1b407e165bf21ea6d905b0ea8ab4aab382a44e74
jenny <jenny@DESKTOP-34IK8HV>
2021-09-07 17:40:19 +0100

    Add project files.

---
+++ fizzbuzz/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                var sb = new StringBuilder("");
+
+                if(i % 3 == 0)
+                {
+                    sb.Append("Fizz");
+                }
+
+                if (i % 5 == 0)
+                {
+                    sb.Append("Buzz");
+                }
+
+                Console.WriteLine(sb);
+            }
+            Console.ReadKey();
+        }
+    }
+}
